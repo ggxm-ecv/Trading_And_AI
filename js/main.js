2257e346f46c0fc656e565d840356394f707cbd4
ggxm-ecv <gautier.maire@mail-ecv.fr>
2021-03-14 17:44:57 +0100

    Init colors + switch color option

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*********************
+// Main
+*********************/
+
+/* Switch Color Mode */
+
+document.querySelector('[data-switch-color]').addEventListener('click', function() {
+  document.body.classList.toggle('dark-mode');
+});
