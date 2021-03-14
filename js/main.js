9c73b9bd0a6e7bebd95c140d977086851585cb0f
ggxm-ecv <gautier.maire@mail-ecv.fr>
2021-03-14 20:45:51 +0100

    switch color mode animation

--- js/main.js
+++ js/main.js
@@ -6,4 +6,6 @@
 
 document.querySelector('[data-switch-color]').addEventListener('click', function() {
   document.body.classList.toggle('dark-mode');
+  document.querySelector('[data-switch-color-svg-sun]').classList.toggle('active');
+  document.querySelector('[data-switch-color-svg-moon]').classList.toggle('active');
 });
