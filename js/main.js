8de44bff3e5dd92053712d2441f4ded18058a632
ggxm-ecv <gautier.maire@mail-ecv.fr>
2021-03-15 15:18:36 +0100

    Init sass breakpoints

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@
 
 document.querySelector('[data-switch-color]').addEventListener('click', function() {
   document.body.classList.toggle('dark-mode');
-  document.querySelector('[data-switch-color-svg-sun]').classList.toggle('active');
-  document.querySelector('[data-switch-color-svg-moon]').classList.toggle('active');
+  document.querySelectorAll('.switch-color-mode__svg').forEach( elem => {
+    elem.classList.toggle('active');
+  });
+
 });
