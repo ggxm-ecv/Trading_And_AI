ebf73e7ac89ef59b5cf9d8568086de1e0ecb90c3
ggxm-ecv <gautier.maire@mail-ecv.fr>
2021-03-18 18:12:59 +0100

    avancement step 1 pattern

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 // Main
 *********************/
 
-/* Switch Color Mode */
+/********* Switch Color Mode *********/
 
 document.querySelector('[data-switch-color]').addEventListener('click', function() {
   document.body.classList.toggle('dark-mode');
@@ -11,3 +11,56 @@ document.querySelector('[data-switch-color]').addEventListener('click', function
   });
 
 });
+
+/********* Step 1 : Experimental Pattern *********/
+
+// Generate Items inside Pattern
+
+let patternWidth = document.querySelector('.step-1__pattern-wrap').offsetWidth;
+let patternHeight = document.querySelector('.step-1__pattern-wrap').offsetHeight;
+
+let itemsPerRow = Math.floor(patternWidth / 40);
+let itemsPerColumn = Math.floor(patternHeight / 40);
+
+let nbItems = itemsPerRow * itemsPerColumn;
+
+
+function insertItemsToPattern(nbItems) {
+
+  let pattern = document.querySelector('.step-1__pattern-wrap');
+
+  while (nbItems > 0) {
+
+    let item = document.createElement('div');
+    item.classList.add('step-1__pattern-item');
+    let itemInner = document.createElement('div');
+    itemInner.classList.add('step-1__pattern-item-inner');
+    item.appendChild(itemInner);
+
+    pattern.appendChild(item);
+
+    nbItems--;
+  }
+
+}
+
+insertItemsToPattern(nbItems);
+
+// Items Animation
+
+// (function() {
+//
+//     let mX, mY, distance,
+//         $element  = $('.step-1__content-wrap .heading');
+//
+//     function calculateDistance(elem, mouseX, mouseY) {
+//         return Math.floor(Math.sqrt(Math.pow(mouseX - (elem.offset().left+(elem.width()/2)), 2) + Math.pow(mouseY - (elem.offset().top+(elem.height()/2)), 2)));
+//     }
+//
+//     $(document).mousemove(function(e) {
+//         mX = e.pageX;
+//         mY = e.pageY;
+//         distance = calculateDistance($element, mX, mY);
+//     });
+//
+// })();
