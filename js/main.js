0ebe7f159f8739e3353f71290d763ed9418488ea
Gautier <gautier@studiozerance.fr>
2021-04-11 17:35:56 +0200

    slider step finition + style btn

--- js/main.js
+++ js/main.js
@@ -13,56 +13,136 @@ document.querySelector('[data-switch-color]').addEventListener('click', function
 });
 
 
-/********* Main Nav *********/
+/*********************
+// Steps
+*********************/
 
-document.querySelector('.header__nav-down').addEventListener('click', event => {
+/********* On Scroll : Slider Steps (gsap) *********/
 
+var currentIndex = 0;
+var isAllowScroll = true;
+var isAllowClick = true;
+var wheelEvents = [];
+var prevTime;
+var steps = document.querySelectorAll('.step');
+
+window.addEventListener('wheel', handleWheel, {
+  passive: false
 });
 
-document.querySelector('.header__nav-up').addEventListener('click', event => {
+function handleWheel(e) {
+  e.preventDefault();
 
-});
+  var time = new Date().getTime();
+  var value = e.wheelDelta;
+  var delta = Math.max(-1, Math.min(1, value));
+  var isVertical = Math.abs(e.wheelDeltaX) < Math.abs(e.wheelDelta);
 
+  if(wheelEvents.length > 149){
+    wheelEvents.shift();
+  }
 
-/********* Slider steps *********/
+  wheelEvents.push(Math.abs(value));
 
-let scrollPos = 0;
-let stepHeight = document.querySelector('.step').offsetHeight;
-let nbOfStep = document.querySelectorAll('.steps > .step').length;
-let targetLastStep = stepHeight * (nbOfStep - 1);
+  var timeDiff = time - prevTime;
+  prevTime = time;
 
-document.querySelector('.steps').addEventListener('scroll', function() {
+  if(timeDiff > 200){
+    wheelEvents = [];
+  }
 
-  let newScrollPos = document.querySelector('.steps').scrollTop;
+  if (isAllowScroll) {
+    var averageEnd = getAverage(wheelEvents, 10);
+    var averageMiddle = getAverage(wheelEvents, 70);
+    var isAccelerating = averageEnd >= averageMiddle;
 
-  // Update Header Nav Up
-  if (newScrollPos > 0) {
+    if(isVertical && isAccelerating) {
+      if (delta < 0) {
+        update(currentIndex + 1);
+      } else {
+        update(currentIndex - 1);
+      }
+    }
+  }
+}
+
+function update(index) {
+
+  // Update Main Nav style
+  if(index > 0) {
     document.querySelector('.header__nav-up').classList.add('active');
   } else {
     document.querySelector('.header__nav-up').classList.remove('active');
   }
-  // Update Header Nav Down
-  if (newScrollPos >= targetLastStep) {
-    document.querySelector('.header__nav-down').classList.remove('active');
-  } else {
+  if(index < (steps.length - 1)) {
     document.querySelector('.header__nav-down').classList.add('active');
-  }
-
-  if (newScrollPos > scrollPos) {
-    // On scroll down
-
-
   } else {
-    // On scroll up
-
-
+    document.querySelector('.header__nav-down').classList.remove('active');
   }
 
-  // saves the new position for iteration.
-	scrollPos = newScrollPos;
+  if(index < 0 || index >= steps.length) {
+    return;
+  }
+
+  // Upadte Steps style
+  steps[currentIndex].classList.remove('show');
+  steps[index].classList.add('show');
+
+  isAllowScroll = false;
+  isAllowClick = false;
+
+  var direction = index > currentIndex ? '-' : '+';
+  gsap.to([steps[currentIndex], steps[index]], 1, {
+    y: direction + '=100%',
+    ease: 'power2.out',
+    onComplete: function() {
+
+      isAllowScroll=true;
+      isAllowClick=true;
+
+      // Update Header Step number
+      document.querySelector('.header__step-current').innerHTML = `${index+1}`;
+
+    }
+  });
+
+  currentIndex = index;
+}
+
+function getAverage(elements, number){
+  var sum = 0;
+  var lastElements = elements.slice(Math.max(elements.length - number, 1));
+
+  for(var i = 0; i < lastElements.length; i++){
+    sum = sum + lastElements[i];
+  }
+
+  return Math.ceil(sum/number);
+}
+
+
+/********* On Nav Click : Slider steps (gsap) *********/
+
+// On Click Next
+const btnStepNext = document.querySelectorAll('[data-step-next]');
+btnStepNext.forEach((el) => {
+  el.addEventListener('click', function(event) {
+    event.preventDefault();
+    if (isAllowClick) {
+      update(currentIndex + 1);
+    }
+  })
 });
-
-
+// On Click Prev
+const btnStepPrev = document.querySelectorAll('[data-step-prev]');
+btnStepPrev.forEach((el) => {
+  el.addEventListener('click', function(event) {
+    event.preventDefault();
+    if (isAllowClick) {
+      update(currentIndex - 1);
+    }
+  })
+});
 
 
 /********* Step 1 : Experimental Pattern *********/
